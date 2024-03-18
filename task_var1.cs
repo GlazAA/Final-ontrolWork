9fdc3dea80ab01bb3255cf2796439d6ff1d872a7
GlazAA <113853477+GlazAA@users.noreply.github.com>
2024-03-18 14:47:22 +0300

    итог

--- task_var1.cs
+++ task_var1.cs
@@ -4,58 +4,51 @@ class Program
 {
     static void Main()
     {
-        string[] inputArray = new string[100]; 
-        int count = 0;
+        string[] array1 = new string[100];
+        string[] array2 = new string[100];
+        int counter = 0;
 
-        Console.WriteLine("Введите строки для формирования массива (для завершения введите 0):");
-
-        string input = Console.ReadLine();
-        while (input != "0")
+        Console.WriteLine("Введите слова и цифры. Для завершения введите 0:");
+        string input;
+        do
         {
-            inputArray[count] = input;
-            count++;
-
             input = Console.ReadLine();
+            if (input != "0")
+            {
+                array1[counter] = input;
+                counter++;
+            }
+            
+        } while (input != "0")
+        int resultCounter = 0;
+
+        
+        for (int i = 0; i < counter; i++)
+        {
+            if (array1[i].Length < 4)
+            {
+                array2[resultCounter] = array1[i];
+                resultCounter++;
+            }
         }
 
-
-
-        string[] resultArray = new string[count];
-        int resultCount = 0;
-
-        for (int i = 0; i < count; i++)
-        {
-            string inputString = inputArray[i];
-            string resultString = "";
-
-            foreach (char c in inputString)
-            {
-                if (char.IsLetterOrDigit(c))
-                {
-                    if (resultString.Length < 3)
+           if (resultCounter == 0)
+           {
+               Console.WriteLine("Все слова или цифры, которые вы ввели, длиннее 3-х символов. Введите короче");
+           }
+                    else
                     {
-                        resultString += c;
-                    }
-                }
-            }
+                        Array.Resize(ref array2, resultCounter);
 
-            if (resultString == "")
-            {
-                resultArray[resultCount] = "символы и цифры не найдены";
-            }
-            else
-            {
-                resultArray[resultCount] = resultString;
-            }
+                        Console.WriteLine("Результат:");
+                        
+                                    for (int i = 0; i < resultCounter; i++)
+                                    {
+                                        Console.WriteLine(array2[i]);
+                                    }
+                     }
 
-            resultCount++;
-        }
-
-        Console.WriteLine("Результат:");
-
-        for (int i = 0; i < resultCount; i++)
-        {
-            Console.WriteLine(resultArray[i]);
-        }
+        Array.Clear(array1, 0, array1.Length);
+        Array.Clear(array2, 0, array2.Length);
     }
 }
